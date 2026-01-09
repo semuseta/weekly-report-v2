0b76d3d516a00b276fb099489d59a64ac6bc116d
semuseta <cem@cemates.de>
2026-01-09 13:24:46 +0000

    Fix: webpack config for PDFKit

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    turbopack: false,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.externals = [...(config.externals || []), 'pdfkit']
+    }
+    return config
   },
 };
 
