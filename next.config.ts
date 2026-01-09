96d4dbc4cd83a621455868e81daa8ee1da3a57a3
semuseta <cem@cemates.de>
2026-01-09 13:26:31 +0000

    Fix: add turbopack config

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.externals = [...(config.externals || []), 'pdfkit']
-    }
-    return config
-  },
+  turbopack: {},
 };
 
 export default nextConfig;
